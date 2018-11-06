f52f79ada366f27ec8031b08cdc1b6b0741c6a67
Shintaro Morikawa <sntr92@gmail.com>
2018-11-06 19:07:25 +0900

    Initial Commit

---
+++ QiitaViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QiitaViewer
+//
+//  Created by shintaro-morikawa on 2018/11/06.
+//  Copyright © 2018 morishin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
